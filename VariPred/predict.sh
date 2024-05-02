49f66fb35d5c81f30ace827923a4ecf8b43401a8
Weining Lin <84018503+wlin16@users.noreply.github.com>
2024-05-02 15:38:57 +0100

    Update predict.sh

--- VariPred/predict.sh
+++ VariPred/predict.sh
@@ -3,7 +3,7 @@
 # target_ds: dataframe name, the name of the data set you would like to get prediction (in .csv format)
 # output_path: file name for the predicted results (in .txt format)
 
-# In this example, we are using the training set named as "target.csv" under the directory "../example/dataset"
+# In this example, we are using the target dataset named as "target.csv" under the directory "../example/dataset"
 df_path="../example/dataset"
 target_ds="target"
 output_name="VariPred_output" 
